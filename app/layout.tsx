83eebbbab76b54179a02d6c780dfcb1f98271dcc
John Suyang <johnsuyang2118@gmail.com>
2025-12-17 21:22:57 +0800

    add light/dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
+        <html className={`${inter.className} antialiased`} lang="en" suppressHydrationWarning>
+            <head />
+            <body>
+                <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
