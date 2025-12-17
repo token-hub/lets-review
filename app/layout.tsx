7a8f6e3b576a5dca019f710912d7ea90e9f835e1
John Suyang <johnsuyang2118@gmail.com>
2025-12-17 21:26:30 +0800

    create common directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/common/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
