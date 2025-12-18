e8b238d2d1dc40f2a90791ae6a74044fe034a7e2
John Suyang <johnsuyang2118@gmail.com>
2025-12-18 22:26:11 +0800

    remove vertical scroll

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Topics from "@/components/topics";
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-[calc(100vh-50px)] overflow-auto p-3">
+        <div className="flex justify-center items-center h-[calc(100vh-77px)] overflow-auto p-3">
             <Topics topics={["Javascript", "Tailwind", "React", "Node", "HTML", "CSS"]} />
         </div>
     );
