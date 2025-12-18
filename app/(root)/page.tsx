f56db22634ab98ff6c06de410c2783c0b40593e5
John Suyang <johnsuyang2118@gmail.com>
2025-12-18 22:25:09 +0800

    add topic components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import Topics from "@/components/topics";
+
 export default function Home() {
-    return <h1>Hello</h1>;
+    return (
+        <div className="flex justify-center items-center h-[calc(100vh-50px)] overflow-auto p-3">
+            <Topics topics={["Javascript", "Tailwind", "React", "Node", "HTML", "CSS"]} />
+        </div>
+    );
 }
---
+++ components/topics.tsx
@@ -0,0 +1,22 @@
+type TopicsProps = {
+    topics?: string[];
+};
+
+const Topics = ({ topics = [] }: TopicsProps) => {
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-2  ">
+            {topics.map((topic) => {
+                return (
+                    <div
+                        key={topic}
+                        className="w-40 h-40 p-3 flex items-center justify-center text-center break-all text-2xl font-bold border rounded-lg shadow-lg hover:shadow-md active:shadow-sm cursor-pointer transition duration-75"
+                    >
+                        {topic}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Topics;
