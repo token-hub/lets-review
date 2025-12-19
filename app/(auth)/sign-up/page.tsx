3f0b3894e1b612f8316720d34f5da1d26552c3a1
John Suyang <johnsuyang2118@gmail.com>
2025-12-19 16:29:56 +0800

    add ZOD and React Hook Form to sign up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,34 +1,67 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { SearchCode } from "lucide-react";
 import Link from "next/link";
+import { Spinner } from "@/components/ui/spinner";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { signUpSchema } from "@/lib/validators";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpDefaultValues } from "@/lib/constants";
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting },
+    } = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: signUpDefaultValues,
+    });
+
+    const onSubmit: SubmitHandler<z.infer<typeof signUpSchema>> = async (data) => {
+        try {
+            console.log(data);
+            // do server action here
+        } catch (err) {
+            // modify the error as needed
+            setError("email", {
+                message: "something went wrong",
+            });
+        }
+    };
+
+    console.log(errors);
     return (
         <div className="h-full flex justify-center items-center h-minus-header">
-            <Card className="w-full max-w-sm ">
-                <CardHeader className="space-y-2 place-items-center">
-                    <Link href="/" className="hover:cursor-pointer">
-                        <SearchCode className="w-18 h-18 m-0 text-yellow-500" />
-                    </Link>
-                    <CardTitle className="text-center text-2xl">Sign Up</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <form>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm">
+                <Card>
+                    <CardHeader className="space-y-2 place-items-center">
+                        <Link href="/" className="hover:cursor-pointer">
+                            <SearchCode className="w-18 h-18 m-0 text-yellow-500" />
+                        </Link>
+                        <CardTitle className="text-center text-2xl">Sign Up</CardTitle>
+                    </CardHeader>
+                    <CardContent>
                         <div className="flex flex-col gap-6">
                             <div className="grid gap-2">
                                 <Label htmlFor="name">
                                     Name <span className="text-red-700">*</span>
                                 </Label>
-                                <Input id="name" type="text" placeholder="john doe" required />
+                                <Input {...register("name")} type="text" placeholder="john doe" />
+                                {errors.name && <p className="text-sm text-red-500">{errors.name.message}</p>}
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="email">
                                     Email <span className="text-red-700">*</span>
                                 </Label>
-                                <Input id="email" type="email" placeholder="m@example.com" required />
+                                <Input {...register("email")} type="email" placeholder="email@gmail.com" />
+                                {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
@@ -36,7 +69,8 @@ const SignUp = () => {
                                         Password <span className="text-red-700">*</span>
                                     </Label>
                                 </div>
-                                <Input id="password" type="password" placeholder="*******" minLength={6} required />
+                                <Input {...register("password")} type="password" placeholder="********" />
+                                {errors.password && <p className="text-sm text-red-500">{errors.password.message}</p>}
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
@@ -44,21 +78,22 @@ const SignUp = () => {
                                         Confirm Password <span className="text-red-700">*</span>
                                     </Label>
                                 </div>
-                                <Input id="con-password" type="password" placeholder="*******" minLength={6} required />
+                                <Input {...register("confirmPassword")} type="password" placeholder="********" />
+                                {errors.confirmPassword && <p className="text-sm text-red-500">{errors.confirmPassword.message}</p>}
                             </div>
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full hover:cursor-pointer">
-                        Login
-                    </Button>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button disabled={isSubmitting} type="submit" className="w-full hover:cursor-pointer">
+                            {isSubmitting && <Spinner />} Submit
+                        </Button>
 
-                    <Link href="/sign-in" className="text-sm text-center text-muted-foreground">
-                        Already have an account? <span className="text-yellow-500 font-bold">Sign In</span>
-                    </Link>
-                </CardFooter>
-            </Card>
+                        <Link href="/sign-in" className="text-sm text-center text-muted-foreground">
+                            Already have an account? <span className="text-yellow-500 font-bold">Sign In</span>
+                        </Link>
+                    </CardFooter>
+                </Card>
+            </form>
         </div>
     );
 };
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const signUpDefaultValues = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+};
