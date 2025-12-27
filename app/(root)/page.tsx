c1300e7fcdd147174312b17692c3c4e6e10145ec
John Suyang <johnsuyang2118@gmail.com>
2025-12-27 18:12:23 +0800

    make topics to change when clicking a box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import Topics from "@/components/topics";
+import QuestionsCount from "@/components/questionsCount";
+import { questionsCountDefaultValue } from "@/lib/constants";
+import { useProgress } from "@/providers/progressProvider";
 
 export default function Home() {
+    const { topic } = useProgress();
+    const progress = null; // can be null or object
+    const hasProgess = Object.keys(progress ?? {}).length > 0;
+
     return (
         <div className="flex justify-center items-center h-[calc(100vh-77px)] overflow-auto p-3">
-            <Topics topics={["Javascript", "Tailwind", "React", "Node", "HTML", "CSS"]} />
+            {hasProgess && <>Question Component here</>}
+            {!hasProgess && !topic && <Topics topics={["Javascript", "React", "Node.js", "Mongodb", "CSS", "Bootsrap"]} />}
+            {!hasProgess && topic && <QuestionsCount questionsCount={questionsCountDefaultValue} />}
         </div>
     );
 }
