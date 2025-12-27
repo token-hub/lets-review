e3326ad5d96b2598ca6e0d65a07f3534d0afe713
John Suyang <johnsuyang2118@gmail.com>
2025-12-27 18:08:12 +0800

    add reusable box component

--- components/box.tsx
+++ components/box.tsx
@@ -1,8 +1,23 @@
+import { useProgress } from "@/providers/progressProvider";
 import { BoxPropsType } from "@/types";
+import { boxOwner } from "@/lib/constants";
 
-const Box = ({ text }: BoxPropsType) => {
+const Box = ({ text, owner }: BoxPropsType) => {
+    const ctx = useProgress();
+
+    function handleClick() {
+        if (owner === boxOwner[0]) {
+            ctx.handleTopic(text);
+        } else {
+            ctx.handleQuestionCount(+text);
+        }
+    }
+
     return (
-        <div className="w-40 h-40 p-3 flex  items-center justify-center text-center break-all text-2xl font-bold border rounded-lg shadow-lg hover:shadow-md active:shadow-sm cursor-pointer transition duration-75 hover:text-yellow-500 hover:border-yellow-500">
+        <div
+            onClick={handleClick}
+            className="w-40 h-40 p-3 flex  items-center justify-center text-center break-all text-2xl font-bold border rounded-lg shadow-lg hover:shadow-md active:shadow-sm cursor-pointer transition duration-75 hover:text-yellow-500 hover:border-yellow-500"
+        >
             {text}
         </div>
     );
--- components/topics.tsx
+++ components/topics.tsx
@@ -1,22 +1,16 @@
-type TopicsProps = {
-    topics?: string[];
-};
+import { memo } from "react";
+import { TopicsProps } from "@/types";
+import Box from "./box";
+import { boxOwner } from "@/lib/constants";
 
 const Topics = ({ topics = [] }: TopicsProps) => {
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2  ">
             {topics.map((topic) => {
-                return (
-                    <div
-                        key={topic}
-                        className="w-40 h-40 p-3 flex  items-center justify-center text-center break-all text-2xl font-bold border rounded-lg shadow-lg hover:shadow-md active:shadow-sm cursor-pointer transition duration-75 hover:text-yellow-500 hover:border-yellow-500"
-                    >
-                        {topic}
-                    </div>
-                );
+                return <Box owner={boxOwner[0]} text={topic} key={topic} />;
             })}
         </div>
     );
 };
 
-export default Topics;
+export default memo(Topics);
