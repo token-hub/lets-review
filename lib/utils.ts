2f92cc92afa46ba609417c19f46ad656650af5ed
John Suyang <johnsuyang2118@gmail.com>
2025-12-29 23:26:18 +0800

    generate guest user id

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { createId } from "@paralleldrive/cuid2";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function getGuestUser() {
+    const user = localStorage.getItem("guestUser");
+
+    if (!user) {
+        const newUser = {
+            id: createId(),
+            isGuest: true,
+        };
+
+        localStorage.setItem("guestUser", JSON.stringify(newUser));
+        return newUser;
+    }
+
+    return JSON.parse(user);
 }
